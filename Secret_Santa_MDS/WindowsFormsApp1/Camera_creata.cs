e3088d2322167ce8013036ed2f1f70d06457d78b
Flashin1912 <92026401+Flashin1912@users.noreply.github.com>
2024-06-06 23:29:45 +0300

    Add files via upload

---
+++ Secret_Santa_MDS/WindowsFormsApp1/Camera_creata.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Camera_creata : Form
+    {
+        public Camera_creata(int participanti,int cod,string nume)
+        {
+            InitializeComponent();
+            Lista_useri.Text += " "+participanti.ToString();
+            nr_useri.Text = nume;
+            if (cod == 0)
+            {
+                
+                Random rnd = new Random();
+                Actual_room_code.Text = rnd.Next(100000, 999999).ToString();
+            }
+            else Actual_room_code.Text = cod.ToString();
+        }
+
+        private void Start_button_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            var Camera_de_final = new Camera_final();
+            Camera_de_final.Show();
+        }
+    }
+}
